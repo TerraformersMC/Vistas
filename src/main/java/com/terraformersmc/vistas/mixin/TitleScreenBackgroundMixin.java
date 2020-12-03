9ac5366098bda266ffc76e97b42b4284f46679c0
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2020-12-03 11:02:54 -0600

    #change "Added panorama overlay support"

--- src/main/java/com/terraformersmc/vistas/mixin/TitleScreenBackgroundMixin.java
+++ src/main/java/com/terraformersmc/vistas/mixin/TitleScreenBackgroundMixin.java
@@ -1,49 +1,70 @@
 package com.terraformersmc.vistas.mixin;
 
-import com.terraformersmc.vistas.Panorama;
-import com.terraformersmc.vistas.config.PanoramaConfig;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.CubeMapRenderer;
-import net.minecraft.client.gui.RotatingCubeMapRenderer;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.terraformersmc.vistas.Panorama;
+import com.terraformersmc.vistas.config.PanoramaConfig;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.CubeMapRenderer;
+import net.minecraft.client.gui.RotatingCubeMapRenderer;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 @Mixin(TitleScreen.class)
-public abstract class TitleScreenBackgroundMixin {
+public abstract class TitleScreenBackgroundMixin extends Screen {
 
-    @Shadow
-    private RotatingCubeMapRenderer backgroundRenderer;
+	protected TitleScreenBackgroundMixin(Text title) {
+		super(title);
+	}
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void VISTAS_initPanoramaChange(CallbackInfo ci) {
-        if (PanoramaConfig.INSTANCE().randomPerScreen) {
-            Panorama.setRandomPanorama();
-        }
-        updateScreen();
-    }
+	@Shadow
+	private RotatingCubeMapRenderer backgroundRenderer;
 
-    @Inject(method = "tick", at = @At("TAIL"))
-    private void VISTAS_tickPanoramaChange(CallbackInfo ci) {
+	@Inject(method = "init", at = @At("TAIL"))
+	private void VISTAS_initPanoramaChange(CallbackInfo ci) {
+		if (PanoramaConfig.INSTANCE().randomPerScreen) {
+			Panorama.setRandomPanorama();
+		}
+		updateScreen();
+	}
 
-        if (PanoramaConfig.INSTANCE().hectic) {
-            Panorama.setRandomPanorama();
+	@Inject(method = "tick", at = @At("TAIL"))
+	private void VISTAS_tickPanoramaChange(CallbackInfo ci) {
 
-            updateScreen();
-        }
-    }
+		if (PanoramaConfig.INSTANCE().hectic) {
+			Panorama.setRandomPanorama();
 
-    private void updateScreen() {
-        if (Panorama.getPanorama() != null) {
-            this.backgroundRenderer = new RotatingCubeMapRenderer(new CubeMapRenderer(Panorama.getPanorama().getId()));
-        } else {
-            this.backgroundRenderer = new RotatingCubeMapRenderer(TitleScreen.PANORAMA_CUBE_MAP);
-        }
-    }
+			updateScreen();
+		}
+	}
+
+	@ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/texture/TextureManager;bindTexture(Lnet/minecraft/util/Identifier;)V", ordinal = 0), index = 0)
+	private Identifier VISTAS_overlayMixin(Identifier defaultOverlay) {
+		if (Panorama.getPanorama() != null) {
+			Identifier overlayId = new Identifier(Panorama.getPanorama().getId().toString() + "_overlay.png");
+			if (this.client.getResourceManager().containsResource(overlayId)) {
+				return overlayId;
+			}
+		}
+		return defaultOverlay;
+	}
+
+	private void updateScreen() {
+		if (Panorama.getPanorama() != null) {
+			this.backgroundRenderer = new RotatingCubeMapRenderer(new CubeMapRenderer(Panorama.getPanorama().getId()));
+		} else {
+			this.backgroundRenderer = new RotatingCubeMapRenderer(TitleScreen.PANORAMA_CUBE_MAP);
+		}
+	}
 
 }
