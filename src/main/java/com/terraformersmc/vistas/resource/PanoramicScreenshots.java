c8b1bea9ec59ea4eb0a52eb7d499ba83ab0302a1
NotRyken <127091011+NotRyken@users.noreply.github.com>
2026-08-02 11:56:50 +0800

    Update to mc1.21.10 (#44)
    - Update to Minecraft 1.21.9/10, thanks Ryken

--- src/main/java/com/terraformersmc/vistas/resource/PanoramicScreenshots.java
+++ src/main/java/com/terraformersmc/vistas/resource/PanoramicScreenshots.java
@@ -13,7 +13,6 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.text.Text;
-
 import java.io.File;
 import java.nio.file.Path;
 import java.text.DateFormat;
@@ -27,7 +26,7 @@ public class PanoramicScreenshots {
 	public static int cooldown = 0;
 
 	public static void registerKeyBinding() {
-		KeyBinding screenshotKey = new KeyBinding("key.vistas.panoramic_screenshot", 'H', "key.categories.misc");
+		KeyBinding screenshotKey = new KeyBinding("key.vistas.panoramic_screenshot", 'H', KeyBinding.Category.MISC);
 		KeyBindingHelper.registerKeyBinding(screenshotKey);
 		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			if (cooldown > 0) {
